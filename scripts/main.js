315a56db84d898d7fea8029a4577a55c7831d4dd
LaurenceRolandJames <laurencerolandjames@gmail.com>
2014-12-06 19:36:47 +0000

    getting flourescent

--- scripts/main.js
+++ scripts/main.js
@@ -7,10 +7,16 @@ $(document).ready( function() {
   function beginRave() { 
     $('body').removeClass('darkness');
     $('#kitchen').removeClass("hidden");
-    var color = '#';
-    var letters = ['FF7F50','FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF','C0C0C0','8B008B','FF1493','FF00FF','FF69B4','FFB6C1']; 
-    color += letters[Math.floor(Math.random() * letters.length)];
-    document.getElementById('kitchen').style.background = color;
+    $('.begin_rave').addClass('hidden');
+    $('.end_rave').removeClass('hidden');
+
+    setInterval(function() {
+         var color = '#';
+         var letters = ['FF7F50','FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF','C0C0C0','8B008B','FF1493','FF00FF','FF69B4','FFB6C1']; 
+         color += letters[Math.floor(Math.random() * letters.length)];
+         document.getElementById('kitchen').style.background = color;
+    }, 30);
+    
   }
 
 });
