1d3a041d445a68860810841a239cefea806249cc
LaurenceRolandJames <laurencerolandjames@gmail.com>
2015-01-17 18:55:23 +0000

    adding habib

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 $(document).ready( function() {
+  console.log('Habib is beautiful');
 
   $('.begin_rave').click(function() {
     beginRave();
@@ -25,15 +26,20 @@ $(document).ready( function() {
          var letters = ['FF7F50','FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF','C0C0C0','8B008B','FF1493','FF00FF','FF69B4','FFB6C1']; 
          color += letters[Math.floor(Math.random() * letters.length)];
          document.getElementById('kitchen').style.background = color;
-    }, 30);
+    }, 1000);
   }
 
   function endRaveMessage(){
     var message;
-    var messages = ['No!', 'No! The party\'s only just started!', 'sausage', 'potato', 'The rave isn\'t over yet.'];
+    var messages = ['No', 'The party\'s only just started', 'sausage', 'potato', 'The rave isn\'t over yet'];
     message = messages[Math.floor(Math.random() * messages.length)];
     alert(message);
   }
+
+  $('.beautiful_habib').click(function() {
+    $(this).addClass('playful_habib');
+    console.log('Habib is beautiful and playful');
+  });
 
 });
 
